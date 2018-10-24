6f37243df432a5cf285c3b5e2ff66f6e39eafaa5
g3bilode <philip.bilodeau@gmail.com>
2018-10-23 23:08:28 -0400

    Replace BP ConquestPlayerState with CPP class

---
+++ Source/Conquest/PlayerCharacter/ConquestPlayerState.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ConquestPlayerState.h"
+#include "UnrealNetwork.h"
+
+void AConquestPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AConquestPlayerState, Gold);
+}
+
+
+
---
+++ Source/Conquest/PlayerCharacter/ConquestPlayerState.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ConquestPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONQUEST_API AConquestPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AConquestPlayerState() : GoldGainMultiplayer(1) {}
+
+	UPROPERTY(BlueprintReadWrite, Category = "Resources", Replicated)
+	int32 Gold;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Resources")
+	int32 GoldGainMultiplayer;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Team")
+	FName TeamName;
+
+};
